dfa0c005dd81068a9149e8e8a09b286ba4b9bde4
Basma Ahmed <Basma00Ahmed@outlook.com>
2021-08-10 21:05:30 +0200

    First version

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+const gitBtn = document.querySelector('#btn-get');
+const gitinput = document.querySelector('#text-input');
+const gitOutput = document.querySelector('#text-output');
+
+const get_api =() => {
+debugger;
+
+   fetch('https://www.swapi.tech/api/people/?name='+gitinput.value, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+        .then(response =>   response.json())
+        .then(data => {
+            debugger;
+            console.log(data.result[0].properties.gender);
+            gitOutput.innerHTML +="Height : "+ data.result[0].properties.height;
+            gitOutput.innerHTML +=" , mass : "+ data.result[0].properties.mass;
+            gitOutput.innerHTML +=" , Gender : "+ data.result[0].properties.gender;      
+            gitOutput.innerHTML +=" , Hair_color : "+ data.result[0].properties.hair_color;
+        })
+        .catch(error => console.log('error', error))
+}
+gitBtn.addEventListener('click', get_api);
