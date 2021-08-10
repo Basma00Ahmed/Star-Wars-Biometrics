501de457745979709c1d10a121f5680cee4efc45
Basma Ahmed <Basma00Ahmed@outlook.com>
2021-08-10 21:42:37 +0200

    edit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,23 @@
+const gitBtn = document.querySelector("#btn-get");
+const gitinput = document.querySelector("#text-input");
+const gitOutput = document.querySelector("#text-output");
 
-const gitBtn = document.querySelector('#btn-get');
-const gitinput = document.querySelector('#text-input');
-const gitOutput = document.querySelector('#text-output');
-
-const get_api =() => {
-debugger;
-
-   fetch('https://www.swapi.tech/api/people/?name='+gitinput.value, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
+const get_api = () => {
+  fetch("https://www.swapi.tech/api/people/?name=" + gitinput.value, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      gitOutput.innerHTML = "";
+      gitOutput.innerHTML += "Height : " + data.result[0].properties.height;
+      gitOutput.innerHTML += " , mass : " + data.result[0].properties.mass;
+      gitOutput.innerHTML += " , Gender : " + data.result[0].properties.gender;
+      gitOutput.innerHTML +=
+        " , Hair_color : " + data.result[0].properties.hair_color;
     })
-
-        .then(response =>   response.json())
-        .then(data => {
-            debugger;
-            console.log(data.result[0].properties.gender);
-            gitOutput.innerHTML +="Height : "+ data.result[0].properties.height;
-            gitOutput.innerHTML +=" , mass : "+ data.result[0].properties.mass;
-            gitOutput.innerHTML +=" , Gender : "+ data.result[0].properties.gender;      
-            gitOutput.innerHTML +=" , Hair_color : "+ data.result[0].properties.hair_color;
-        })
-        .catch(error => console.log('error', error))
-}
-gitBtn.addEventListener('click', get_api);
+    .catch((error) => console.log("error", error));
+};
+gitBtn.addEventListener("click", get_api);
